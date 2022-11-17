fc102f8b25bc6b4c4e9a634fe27090097fb15236
bablu <bablu@nimbuspost.com-at-664880521685>
2022-11-17 17:20:46 +0530

    code updated

---
+++ footer.php
@@ -0,0 +1,69 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+
+     <div class="footer-top">
+         <div class="container">
+             <div class="row">
+
+                 <div class="col-lg-3 col-md-6 footer-contact">
+                     <h3><a href="index.php"><img src="./assets/img/logo/f-logo.png" class="f-logo" alt=""></a></h3>
+                     <br />
+                     <p>
+                         1018, 3rd Floor, WestField Complex,
+                         Ghod Dod Road, Surat - 395001 <br /><br />
+                         <strong>Phone:</strong> <a href="tel:9824048393">9824-048-393</a><br>
+                         <strong>Email:</strong> <a href="mailto:info@gurukultraining.com">info@gurukultraining.com</a><br>
+                     </p>
+                 </div>
+
+                 <div class="col-lg-2 col-md-6 footer-links">
+                     <h4>Useful Links</h4>
+                     <ul>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                     </ul>
+                 </div>
+
+                 <div class="col-lg-3 col-md-6 footer-links">
+                     <h4>Our Services</h4>
+                     <ul>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Business Training Program</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Team Training Program</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Business Consultancy</a></li>
+                         <li><i class="bx bx-chevron-right"></i> <a href="#">Customize Support For Business</a></li>
+                     </ul>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6 footer-newsletter">
+                     <h4>Join Our Newsletter</h4>
+                     <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                     <form action="" method="post">
+                         <input type="email" name="email"><input type="submit" value="Subscribe">
+                     </form>
+                 </div>
+
+             </div>
+         </div>
+     </div>
+
+     <div class="footer-bottom">
+         <div class="container d-md-flex align-items-center">
+
+             <div class="me-md-auto text-center text-md-start">
+                 <div class="copyright">
+                     &copy; Copyright 2022 <strong><span>Gurukul Training</span></strong>. All Rights Reserved
+                 </div>
+
+             </div>
+             <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                 <a href="https://www.facebook.com/pg/gurukultrainingandconsultancy/about/" class="facebook"><i class="bx bxl-facebook"></i></a>
+                 <a href="https://twitter.com/chalogurukul" class="twitter"><i class="bi bi-twitter"></i></a>
+                 <a href="https://www.instagram.com/gurukultrainingandconsultancy/" class="instagram"><i class="bi bi-instagram"></i></a>
+                 <a href="https://www.youtube.com/channel/UCSKwEDGN_sNsgd2_XZVn4Yg" class="youtube"><i class="bi bi-youtube"></i></i></a>
+             </div>
+         </div>
+     </div>
+ </footer><!-- End Footer -->
---
+++ header.php
@@ -0,0 +1,78 @@
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+      <div class="container d-flex justify-content-between">
+          <div class="contact-info d-flex align-items-center">
+              <i class="bi bi-envelope"></i> <a href="mailto:info@gurukultraining.com">info@gurukultraining.com</a>
+              <i class="bi bi-phone"></i> <a href="tel:9824669700">9824-669-700</a>
+          </div>
+          <div class="d-none d-lg-flex social-links align-items-center">
+              <a href="https://www.facebook.com/pg/gurukultrainingandconsultancy/about/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://twitter.com/chalogurukul" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="https://www.instagram.com/gurukultrainingandconsultancy/" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="https://www.youtube.com/channel/UCSKwEDGN_sNsgd2_XZVn4Yg" class="youtube"><i class="bi bi-youtube"></i></i></a>
+          </div>
+      </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center">
+
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <a href="index.php" class="logo me-auto"><img src="assets/img/logo/logo.png" alt="" class="img-fluid"></a>
+
+          <nav id="navbar" class="navbar order-last order-lg-0">
+              <ul>
+                  <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                  <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+                  <!-- <li><a class="nav-link scrollto" href="#services">Services</a></li> -->
+                  <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+                      <ul>
+                          <li class="dropdown"><a href="#"><span>Business Training Program</span> <i class="bi bi-chevron-right"></i></a>
+                              <ul>
+                                  <li><a href="#">One Day Powerpack Seminars</a></li>
+                                  <li><a href="#">Business Building Strategy</a></li>
+                                  <li><a href="#">Leadership Development Program</a></li>
+                                  <li><a href="#">Retail Business Management</a></li>
+                              </ul>
+                          </li>
+                          <li class="dropdown"><a href="#"><span>Team Training Program</span> <i class="bi bi-chevron-right"></i></a>
+                              <ul>
+                                  <li><a href="#">Gurukul For Managers</a></li>
+                                  <li><a href="#">Customize Staff Training</a></li>
+                                  <li><a href="#">Sales Training</a></li>
+                                  <li><a href="#">Motivation Workshop</a></li>
+                              </ul>
+                          </li>
+                          <li class="dropdown"><a href="#"><span>Business Consultancy</span> <i class="bi bi-chevron-right"></i></a>
+                              <ul>
+                                  <li><a href="#">Business Management Consultancy</a></li>
+                                  <li><a href="#">Sales Management Consultancy</a></li>
+                                  <li><a href="#">Strategic Consultancy</a></li>
+                                  <li><a href="#">HR Management Consultancy</a></li>
+                                  <li><a href="#">Startup Consultancy</a></li>
+                              </ul>
+                          </li>
+                          <li class="dropdown"><a href="#"><span>Customize Support For Business</span> <i class="bi bi-chevron-right"></i></a>
+                              <ul>
+                                  <li><a href="#">Personal Coaching</a></li>
+                                  <li><a href="#">Mentoring</a></li>
+                                  <li><a href="#">Team Training</a></li>
+                                  <li><a href="#">Requirement</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+
+                  
+                  </li>
+                  <li><a class="nav-link scrollto" href="#">Career</a></li>
+
+                  <!-- <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li> -->
+              </ul>
+              <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+
+          <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Contact Us</a>
+
+      </div>
+  </header><!-- End Header -->
